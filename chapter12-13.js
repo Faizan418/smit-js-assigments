aac9383a2a47dbc2e12d846f28cafb7fcf996d9c
Muhammad Faizan Soomro <141871174+Faizan418@users.noreply.github.com>
2024-09-23 15:02:23 +0500

    done

--- chapter12-13.js
+++ chapter12-13.js
@@ -1,20 +1,18 @@
 
 
+let time = prompt("Enter time in 24-hour format") || "0000";
 
-let ampm = prompt("Enter AM or PM");
-let time = prompt("Enter time in 12-hour format");
-
-if(ampm === 'AM' && time >= 5 && ampm === 'AM' && time <= 12){
+if(time >= "0000" && time < "1200"){
     document.write("Good morning..!");
-}else if(ampm === 'PM' && time >= 1 && ampm === 'PM' && time <= 5){
+}
+else if(time >= "1200" && time < "1700"){
     document.write("Good afternoon..!");
-}else if(ampm === 'PM' && time >= 6 && ampm === 'PM' && time <= 8){
-    document.write("Good evening..!");
-}else if(ampm === 'PM' && time >= 9 && ampm === 'PM' && time <= 11){
-    document.write("Good night..!");
-}else if(ampm === 'AM' && time >= 1 && ampm === 'AM' && time <= 4){
-    document.write("Mid night..!");
-}else{
-    document.write("Invalid time or AM/PM entered....");
 }
 
+else if(time >= "1700" && time < "2100"){
+    document.write("Good evening..!");
+}
+
+else if(time >= "2100" && time < "2359"){
+    document.write("Good night..!");
+}
